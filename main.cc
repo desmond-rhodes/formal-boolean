c9c7026df55274b7c9ed978e858b08c749a8bfca
Desmond Rhodes <desmond.rhodes@outlook.com>
2021-09-13 16:27:12 +0700

    Encode operators to functions

--- main.cc
+++ main.cc
@@ -1,4 +1,17 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
+void parse_sanity();
+void parse_stack();
+void evaluate_matrix();
+
+bool negation(bool);
+bool conjunction(bool, bool);
+bool disjunction_inclusive(bool, bool);
+bool disjunction_exclusive(bool, bool);
+bool implication(bool, bool);
+bool equivalence(bool, bool);
 
 int main(int argc, char* argv[]) {
 	std::ios_base::sync_with_stdio(false);
@@ -6,4 +19,37 @@ int main(int argc, char* argv[]) {
 
 	std::cout << "Hello, world!\n";
 	return 0;
+}
+
+void parse_sanity() {
+}
+
+void parse_stack() {
+}
+
+void evaluate_matrix() {
+}
+
+bool negation(bool a) {
+	return !a;
+}
+
+bool conjunction(bool a, bool b) {
+	return a && b;
+}
+
+bool disjunction_inclusive(bool a, bool b) {
+	return a || b;
+}
+
+bool disjunction_exclusive(bool a, bool b) {
+	return a ^ b;
+}
+
+bool implication(bool a, bool b) {
+	return disjunction_inclusive(negation(a), b);
+}
+
+bool equivalence(bool a, bool b) {
+	return conjunction(implication(a, b), implication(b, a));
 }
